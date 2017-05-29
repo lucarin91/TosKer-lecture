c02fb8b810e46492c3bdfc78dde94e67f58062af
lucarin91 <lucarin91@gmail.com>
2017-05-29 12:04:18 +0200

    add all slide set

---
+++ seminar/compile.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+CXX=xelatex
+TITLE=TosKer_slides
+AUTHOR=Luca_Rinaldi
+DATE=$(date +%F)
+
+# rm *.aux
+# rm *.bib
+# rm *.log
+# rm *.out
+# rm *.nav
+$CXX main.tex
+# $CXX main.tex
+# bibtex main
+# $CXX main.tex
+# $CXX main.tex
+
+# NAME=$TITLE-$AUTHOR
+# NAME_DATA=$NAME[$DATE]
+# rm ../$NAME*
+# cp main.pdf ../$NAME.pdf
+# mv main.pdf ../$NAME_DATA.pdf
