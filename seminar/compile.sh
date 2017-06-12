17c8db1d512df58df55c02c5f438859d79d6d3b0
lucarin91 <lucarin91@gmail.com>
2017-06-12 18:17:38 +0200

    update pdfs

--- seminar/compile.sh
+++ seminar/compile.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 CXX=xelatex
-TITLE=TosKer_slides
+TITLE=TosKer_seminar_slides
 AUTHOR=Luca_Rinaldi
-DATE=$(date +%F)
+# DATE=$(date +%F)
 
-# rm *.aux
-# rm *.bib
-# rm *.log
-# rm *.out
-# rm *.nav
+rm *.aux
+rm *.bib
+rm *.log
+rm *.out
+rm *.nav
 $CXX main.tex
-# $CXX main.tex
+$CXX main.tex
 # bibtex main
 # $CXX main.tex
 # $CXX main.tex
 
-# NAME=$TITLE-$AUTHOR
+NAME=$TITLE-$AUTHOR
 # NAME_DATA=$NAME[$DATE]
-# rm ../$NAME*
-# cp main.pdf ../$NAME.pdf
+rm $NAME*
+mv main.pdf $NAME.pdf
 # mv main.pdf ../$NAME_DATA.pdf
--- thesis/compile.sh
+++ thesis/compile.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 CXX=xelatex
-TITLE=TosKer_slides
+TITLE=TosKer_thesis_slides
 AUTHOR=Luca_Rinaldi
-DATE=$(date +%F)
+# DATE=$(date +%F)
 
 rm *.aux
 rm *.bib
 rm *.log
 rm *.out
+rm *.nav
 $CXX main.tex
 $CXX main.tex
 # bibtex main
@@ -15,7 +16,7 @@ $CXX main.tex
 # $CXX main.tex
 
 NAME=$TITLE-$AUTHOR
-NAME_DATA=$NAME[$DATE]
-rm ../$NAME*
-cp main.pdf ../$NAME.pdf
-mv main.pdf ../$NAME_DATA.pdf
+# NAME_DATA=$NAME[$DATE]
+rm $NAME*
+mv main.pdf $NAME.pdf
+# mv main.pdf ../$NAME_DATA.pdf
